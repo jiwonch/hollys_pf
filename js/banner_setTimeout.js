0456922eecb9103484e2bb2c6c44c43e5565f0b0
JIWON CHOI <rizjw26@gmail.com>
2022-05-27 16:47:47 +0900

    20220527_헤더 아이콘 추가 배너 추가

--- js/banner_setTimeout.js
+++ js/banner_setTimeout.js
@@ -1,12 +1,12 @@
 var ct=0;
-        var imgs = new Array("img/index/banner_1.jpg", "img/index/banner_2.jpg", "img/index/banner_3.jpg", "img/index/banner_4.jpg", "img/index/banner_5.jpg", "img/index/banner_6.jpg");
+        var imgs = new Array("img/index/banner_1.jpg", "img/index/banner_2.jpg", "img/index/banner_3.jpg", "img/index/banner_4.jpg", "img/index/banner_5.jpg", "img/index/banner_6.jpg","img/index/banner_7.jpg","img/index/banner_8.jpg");
         setTimeout(progress, 2000);
         function progress(){
             var ch=document.getElementById('w8_banner');
             ch.src=imgs[ct];
             ct++;
             setTimeout(progress, 2000);
-            if(ct>5){
+            if(ct>imgs.length-1){
                 ct=0;
             }
         }
